25d88bfdbcc2cdcb5de162de95c896ee7a656f13
Emmanuel Dada <altshiftctrl6@gmail.com>
2022-10-25 16:37:13 +0100

    solution

--- twosums.py
+++ twosums.py
@@ -10,4 +10,4 @@ class Solution:
         for i in  range(len(nums) - 1):
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
-                    
+                    return ([i, j])
