525dfc849a0c5346a006df694dbcb3f2046843a9
Emmanuel Dada <altshiftctrl6@gmail.com>
2022-10-25 16:38:39 +0100

    commit

--- twosums.py
+++ twosums.py
@@ -5,7 +5,7 @@
 # You can return the answer in any order.
 
 
-
+#Using double forloop method
 class Solution:
     def twoSum(self, nums:List[int], target:int)->List[int]:
         for i in  range(len(nums) - 1):
