bc3789b13b627ab990b93b305ab527aff88569b3
Emmanuel Dada <altshiftctrl6@gmail.com>
2022-10-27 17:32:55 +0100

    modified

--- add-two-numbers.py
+++ add-two-numbers.py
@@ -1,1 +1,1 @@
-# You are given two non-empty linked lists representing two non-negative integers.
+# You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order,
